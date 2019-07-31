cae4f47536eee14ea25c055e00bf8d04c1dec467
axkeson <axkeson@gmail.com>
2019-07-31 10:44:49 +0800

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -19,6 +19,13 @@ class UsersController extends Controller
         ]);
     }
 
+    public function index()
+    {
+        $users = User::paginate(10);
+
+        return view('users.index', compact('users'));
+    }
+
     /**
      * @param User $user
      *
